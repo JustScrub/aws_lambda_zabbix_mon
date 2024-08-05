f6ce149fbb670e01cba5977674b6aa57ae3e3d2f
Jakub Mladý <jakub.mlady@aimtec.cz>
2024-08-05 13:28:47 +0200

    fix update priority script

--- scripts/lambda_update_priority.py
+++ scripts/lambda_update_priority.py
@@ -1,9 +1,9 @@
 from pyzabbix import ZabbixAPI
-from utility_scripts.zbx_auto_discover import auto_discover
-from zapi import LambdaPriority
+from .utility_scripts.zbx_auto_discover import auto_discover
+from .zapi import LambdaPriority
 import boto3
 import json, sys, os
-from config import ZBX_SUFFIX, AWS_PRIO_TAG
+from .config import ZBX_SUFFIX, AWS_PRIO_TAG
 
 def zbx_delete_triggers(zapi,function_name,suffix=ZBX_SUFFIX):
     # get the trigger IDs
@@ -36,16 +36,17 @@ def update_priority(zab_addr,function_name,priority:LambdaPriority):
 
     zbx_delete_triggers(zapi,function_name,ZBX_SUFFIX)
     aws_lambda_set_prio_tag(lambda_client,function_name,priority)
-    auto_discover([(function_name,priority.value)],zab_addr,ZBX_SUFFIX)
+    auto_discover([(function_name,priority.value)],(zab_addr[0],10051),ZBX_SUFFIX)
 
 
 if __name__=="__main__":
 
     if len(sys.argv) < 3:
-        print("usage: python3 lambda_update_priority <function_name> <priority> [<frontend_host>=localhost] [<frontend_port>=80]")
+        print("usage: python3 -m scripts.lambda_update_priority <function_name> <priority> [<frontend_host>=localhost] [<frontend_port>=80] [<server_port>=10051]")
         print("<function_name>: name of the AWS Lambda function")
         print("<priority>: new valid priority number for the function")
         print("<frontend_host>, <frontend_port>: IP or DNS name and port of the Zabbix Frontend")
+        print("<server_port>: port of the Zabbix Server, at same IP/DNS name as Frontend")
         exit(1)
 
     function_name = sys.argv[1]
